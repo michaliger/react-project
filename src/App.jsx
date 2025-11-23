f787be12fcc388d1fb8f2e67db51ca766768fe37
michaliger <michal6761991@gmail.com>
2025-11-24 01:56:00 +0200

    add app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/Header'
+import HomePage from './pages/HomePage'
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <HomePage />
+    </div>
+  )
+}
